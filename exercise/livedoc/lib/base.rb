8c26c771bc04aac14d74de372dd11b4fa7811319
andyl <andy@r210.com>
2018-05-04 11:26:57 -0700

    Refactor data directory

--- exercise/livedoc/lib/base.rb
+++ exercise/livedoc/lib/base.rb
@@ -1,4 +1,4 @@
 EXERCISE_DIR = File.expand_path("../", File.dirname(__FILE__))
 
-DOC_PATH = "../../data/gutenberg/meditations.txt"
+DOC_PATH = "../../data/gutenberg/Meditations.txt"
 DOC_FILE = File.expand_path(DOC_PATH, EXERCISE_DIR)
